6ed2cdeb5364ee4ad9296d783bdaa32a4e27c874
Alefyp <alejndraprz@gmail.com>
2021-07-27 15:29:50 -0500

    Background styles and Footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,21 @@
 import LegalAnnouncement from "../LegalAnnouncement/LegalAnnouncement";
+import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.container}>
+      <p>Powered by sushi and developed by Alefy</p>
+      <p>
+        This is a personal project developed during the Globant React and React
+        Native Bootcamp | 2021 You can find the{" "}
+        <a
+          href="https://github.com/alefyp/teemates"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          repository available here.
+        </a>
+      </p>
       <LegalAnnouncement />
     </footer>
   );
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,16 +2,56 @@ import styles from "./Landing.module.scss";
 
 function Landing() {
   return (
-    <section>
-      <img
-        src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/es-mx/production/es-mx/static/support-d63ae08baf517425864ddc020a5871d5.png"
-        alt="Akali"
-      />
-      <img
-        src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/es-mx/production/es-mx/static/assassin-d64d3ffdda15e1eed637aefe6a2c7fee.png"
-        alt="Thresh"
-      />
-    </section>
+    <div className={styles.container}>
+      <section className={styles.container__intro}>
+        <div>
+          <h1>Welcome to Teemates!</h1>
+          <p>
+            League of Legends is a team-based strategy game where two teams of
+            five powerful champions face off to destroy the other’s base. Choose
+            from over 140 champions to make epic plays, secure kills, and take
+            down towers as you battle your way to victory.
+          </p>
+        </div>
+
+        <img
+          className={styles.container__img__thresh}
+          src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/es-mx/production/es-mx/static/support-d63ae08baf517425864ddc020a5871d5.png"
+          alt="Thresh"
+          role="presentation"
+        />
+      </section>
+
+      <section className={styles.container__power}>
+        <h2>POWER UP YOUR CHAMP </h2>
+        <p>
+          Champions get stronger by earning experience to level up and gold to
+          buy more powerful items as the game progresses. Staying on top of
+          these two factors is crucial to overpowering the enemy team and
+          destroying their base.
+        </p>
+      </section>
+
+      <section className={styles.container__experience}>
+        <div>
+          <h2>Ear Experience!</h2>
+          <p>
+            When champions earn a certain amount of experience, they level up
+            and can unlock or strengthen abilities and raise their base stats.
+            Gain experience by killing enemy units and champions, assisting in a
+            kill, and destroying defense structures.
+          </p>
+          <button>Meet them all!</button>
+        </div>
+
+        <img
+          className={styles.container__img__akali}
+          src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/es-mx/production/es-mx/static/assassin-d64d3ffdda15e1eed637aefe6a2c7fee.png"
+          alt="Akali"
+          role="presentation"
+        />
+      </section>
+    </div>
   );
 }
 
