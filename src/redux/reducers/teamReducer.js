841b42bb09d80f6a8588ecce2ea30eb875f4144e
Alefyp <alejndraprz@gmail.com>
2021-07-29 15:34:03 -0500

    Reducer update to up to 5 champions condition

--- src/redux/reducers/teamReducer.js
+++ src/redux/reducers/teamReducer.js
@@ -4,6 +4,10 @@ import initialState from "./initialState";
 export default function teamReducer(state = initialState.team, action) {
   switch (action.type) {
     case actionTypes.ADD_CHAMPION:
+      if (state.length === 5) {
+        alert("Oops! You already picked 5 champs!");
+        return state;
+      }
       return [...state, { ...action.champion }];
     case actionTypes.REMOVE_CHAMPION:
       return state.filter((champ) => champ.id !== action.champion.id);
