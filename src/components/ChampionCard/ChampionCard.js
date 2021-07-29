ae2327d65a31830905f50a1478080200a38628ec
Alefyp <alejndraprz@gmail.com>
2021-07-29 09:47:15 -0500

    Card container with space for button utility

--- src/components/ChampionCard/ChampionCard.js
+++ src/components/ChampionCard/ChampionCard.js
@@ -1,14 +1,21 @@
 import styles from "./ChampionCard.Module.scss";
 const ChampionCard = ({ champion }) => {
+  console.log(champion.tags[0]);
   const imageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`;
 
   return (
     <div className={styles.container}>
       <img src={imageUrl} alt={champion.name} />
 
-      <div>
-        <h3>{champion.name}</h3>
-        {/* <p>{champion.Tags[0]}</p> */}
+      <div className={styles.utility}>
+        <div>
+          <h4>{champion.name}</h4>
+          <p>
+            {" "}
+            {champion.tags[0]} | {champion.tags[1]}
+          </p>
+        </div>
+        <div>⭐</div>
       </div>
     </div>
   );
